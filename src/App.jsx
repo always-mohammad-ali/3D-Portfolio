7023d52685c8eac30512dbedac3100113ecd6b8e
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-10-16 10:32:15 +0600

    resource and library installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 
+// Best ways to install libraries from terminal:
+// npm install gsap @gsap/react three @react-three/fiber @react-three/drei @react-three/postprocessing
 
 function App() {
   
 
   return (
-    <>
+    <main>
+
+      <div>
+          <h1 className="blue-100">Welcome to my 3D Portfolio</h1>
+      </div>
       
-    </>
+    </main>
   )
 }
 
