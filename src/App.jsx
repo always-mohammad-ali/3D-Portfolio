a249e73f12f630b54be594b412cc4e59c47efae7
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-10-16 12:41:25 +0600

    header section partially done

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@
 // Best ways to install libraries from terminal:
 // npm install gsap @gsap/react three @react-three/fiber @react-three/drei @react-three/postprocessing
 
+import Hero from "./components/Sections/Hero"
+
 function App() {
   
 
   return (
     <main>
 
-      <div>
-          <h1 className="blue-100">Welcome to my 3D Portfolio</h1>
-      </div>
+      <Hero></Hero>
       
     </main>
   )
